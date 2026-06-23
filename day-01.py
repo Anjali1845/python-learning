d85ccec9d3053f879ee77ee2d7992cc62dcd7910
Anjali <mokaanjali1845@gmail.com>
2026-06-24 00:30:25 +0530

    python basics testing

---
+++ day-01.py
@@ -0,0 +1,28 @@
+print("Hello, World!")
+
+print("Hello, World!")
+print("Hello, World!")
+
+
+
+boolean = True
+if boolean:
+    print("True")
+else:
+    print("False")
+
+
+print(boolean)
+
+print(type(boolean))
+
+number = 10
+
+print(number)
+
+print(type(number))
+
+number = 10.5
+
+print(number)
+print(type(number))
